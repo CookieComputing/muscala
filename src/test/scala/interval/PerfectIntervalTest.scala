8c0dd4142e0456e191b17e729ca7bc5efb166ec0
Kevin Hui <khui.cs@gmail.com>
2021-04-02 20:01:59 -0400

    [PerfectIntervalTest] Modify PerfectIntervalTest to use ScalaTest

--- src/test/scala/interval/PerfectIntervalTest.scala
+++ src/test/scala/interval/PerfectIntervalTest.scala
@@ -1,36 +1,44 @@
 package interval
 import note.Note
 import note.NoteTest.noteGen
-import org.scalacheck.Properties
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 /**
   * Represents tests for the Perfect interval.
   */
-object PerfectIntervalTest extends Properties("PerfectInterval") {
-  import org.scalacheck.Prop.forAll
-
-  property("PerfectUnisonIsTheSameNote") = forAll(noteGen) { note: Note =>
-    note.perfect.unison == note
+class PerfectIntervalTest extends AnyPropSpec with ScalaCheckPropertyChecks {
+  property("PerfectUnisonIsTheSameNote") {
+    forAll(noteGen) { note: Note =>
+      assert(note.perfect.unison == note)
+    }
   }
 
-  property("PerfectFourthIsAHalfStepAboveMajorThird") = forAll(noteGen) {
-    note: Note =>
-      note.perfect.fourth.rank == note.major.third.rank + 1
+  property("PerfectFourthIsAHalfStepAboveMajorThird") {
+    forAll(noteGen) { note: Note =>
+      assert(note.perfect.fourth.rank == note.major.third.rank + 1)
+    }
   }
 
-  property("PerfectFourthIsAWholeStepBelowPerfectFifth") = forAll(noteGen) {
-    note: Note =>
-      note.perfect.fourth.rank == note.perfect.fifth.rank - 2
+  property("PerfectFourthIsAWholeStepBelowPerfectFifth") {
+    forAll(noteGen) { note: Note =>
+      assert(note.perfect.fourth.rank == note.perfect.fifth.rank - 2)
+    }
   }
 
-  property("PerfectFifthIsAStackedMajorAndMinorThird") = forAll(noteGen) {
-    note: Note =>
-      note.major.third.minor.third.rank == note.perfect.fifth.rank &&
-      note.minor.third.major.third.rank == note.perfect.fifth.rank
+  property("PerfectFifthIsAStackedMajorAndMinorThird") {
+    forAll(noteGen) { note: Note =>
+      assert(
+        note.major.third.minor.third.rank == note.perfect.fifth.rank &&
+          note.minor.third.major.third.rank == note.perfect.fifth.rank)
+    }
   }
 
-  property("PerfectOctaveIsAnOctaveAboveRoot") = forAll(noteGen) { note: Note =>
-    note.octave == note.perfect.octave.octave - 1 &&
-    note.rank == note.perfect.octave.rank - Note.halfStepsInOctave
+  property("PerfectOctaveIsAnOctaveAboveRoot") {
+    forAll(noteGen) { note: Note =>
+      assert(
+        note.octave == note.perfect.octave.octave - 1 &&
+          note.rank == note.perfect.octave.rank - Note.halfStepsInOctave)
+    }
   }
 }
