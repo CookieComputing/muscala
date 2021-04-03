3ff70d70a91e3cfbe3c548ebc084786b15a6d278
Kevin Hui <khui.cs@gmail.com>
2021-04-02 21:02:48 -0400

    [MajorInterval] Refactor tests to use distance() instead of rank

--- src/test/scala/interval/MajorIntervalTest.scala
+++ src/test/scala/interval/MajorIntervalTest.scala
@@ -10,37 +10,37 @@ import note.NoteTest.noteGen
 class MajorIntervalTest extends AnyPropSpec with ScalaCheckPropertyChecks {
   property("a major second is a whole step above the root") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.second.rank == note.rank + 2)
+      assert(Note.distance(note, note.major.second) == 2)
     }
   }
 
   property("a major third is a whole step above a major second") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.third.rank == note.major.second.rank + 2)
+      assert(Note.distance(note.major.second, note.major.third) == 2)
     }
   }
 
   property("a major third is a half step below a perfect fourth") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.third.rank == note.perfect.fourth.rank - 1)
+      assert(Note.distance(note.major.third, note.perfect.fourth) == 1)
     }
   }
 
   property("a major sixth is a whole step above a perfect fifth") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.sixth.rank == note.perfect.fifth.rank + 2)
+      assert(Note.distance(note.perfect.fifth, note.major.sixth) == 2)
     }
   }
 
   property("a major sixth is a whole step below a major seventh") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.sixth.rank == note.major.seventh.rank - 2)
+      assert(Note.distance(note.major.sixth, note.major.seventh) == 2)
     }
   }
 
   property("a major seventh is a half step below the octave") {
     forAll(noteGen) { note: Note =>
-      assert(note.major.seventh.rank == note.perfect.octave.rank - 1)
+      assert(Note.distance(note.major.seventh, note.perfect.octave) == 1)
     }
   }
 }
