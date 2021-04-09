ad386639d6eb946c00323e74530579247d896865
Kevin Hui <khui.cs@gmail.com>
2021-04-09 10:12:32 -0400

    [DiatonicInterval] Add diatonic intervals

--- src/main/scala/interval/diatonic/DiatonicInterval.scala
+++ src/main/scala/interval/diatonic/DiatonicInterval.scala
@@ -1,12 +1,123 @@
 package interval.diatonic
-import interval.Interval
-import key.{Key, MajorKey}
+import key.Key
 import note.Note
+
+import scala.annotation.tailrec
 
 /**
   * Provides utility methods for working with intervals at a diatonic level. This
   * differs from the absolute intervals module by providing matching the names
   * of an interval to the scale degree that would be expected in a given key.
-  * For instance, if a diatonic third was requested for the key of "C" for
+  * For instance, if a diatonic third was requested for the key of "C" for a
+  * note that is enharmonic to "E", we would expect "G". This module does not
+  * work for notes that do not fit in the key, such as accidentals in the key.
   */
-object DiatonicInterval {}
+object DiatonicInterval {
+
+  /**
+    * Represents a note a unison interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a unison interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def unison(note: Note)(implicit key: Key): Option[Note] = move(note, 0, key)
+
+  /**
+    * Represents a note a second interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a second interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def second(note: Note)(implicit key: Key): Option[Note] = move(note, 1, key)
+
+  /**
+    * Represents a note a third interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a third interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def third(note: Note)(implicit key: Key): Option[Note] = move(note, 2, key)
+
+  /**
+    * Represents a note a fourth interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a fourth interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def fourth(note: Note)(implicit key: Key): Option[Note] = move(note, 3, key)
+
+  /**
+    * Represents a note a fifth interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a fifth interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def fifth(note: Note)(implicit key: Key): Option[Note] = move(note, 4, key)
+
+  /**
+    * Represents a note a sixth interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a sixth interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def sixth(note: Note)(implicit key: Key): Option[Note] = move(note, 5, key)
+
+  /**
+    * Represents a note a seventh interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a seventh interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def seventh(note: Note)(implicit key: Key): Option[Note] = move(note, 6, key)
+
+  /**
+    * Represents a note a octave interval away from this note
+    * @param note the note to base the diatonic interval from
+    * @param key the key to extract diatonic intervals from
+    * @return a note a octave interval away, if the given note is enharmonic
+    *         with a note from the key, else None.
+    */
+  def octave(note: Note)(implicit key: Key): Option[Note] =
+    move(note, 7, key).flatMap(n => Note(n.name, n.octave + 1))
+
+  /**
+    * Helper to perform the actual movement from a note to another note in
+    * the key. If the note does not exist in the key before moving or after
+    * movement, return None
+    * @param note the root note to base the diatonic interval from
+    * @param interval how far the desired note is from the given note
+    * @param key the key to extract diatonic intervals from
+    * @return a note if the given note is enharmonic, None otherwise
+    */
+  private def move(note: Note, interval: Int, key: Key): Option[Note] =
+    if (inKey(note, key)) {
+      val origin = key.degrees
+        .map(Note(_).get.nearestNote)
+        .indexWhere(n => n.name == note.nearestNote.name)
+
+      val dest = (origin + interval) % key.degrees.size
+      val destNote = Note(key.degrees(dest)).get
+      Some(sharpUntilSimilar(note, destNote).copy(name = key.degrees(dest)))
+    } else None
+
+  // Helper to check if a note is in key
+  private def inKey(note: Note, key: Key): Boolean =
+    key.degrees
+      .map(Note(_).get.nearestNote)
+      .exists(_.name == note.nearestNote.name)
+
+  // Sharp a note until the note matches the desired note string
+  @tailrec
+  private def sharpUntilSimilar(note: Note, goal: Note): Note =
+    if ((note.rank % Note.halfStepsInOctave) == (goal.rank % Note.halfStepsInOctave))
+      note
+    else
+      sharpUntilSimilar(note.sharp, goal)
+}
---
+++ src/test/scala/interval/diatonic/DiatonicIntervalTest.scala
@@ -0,0 +1,111 @@
+package interval.diatonic
+import key.{Key, MajorKeyTest, MinorKeyTest}
+import note.{Note, NoteTest}
+import org.scalacheck.Gen
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class DiatonicIntervalTest extends AnyPropSpec with ScalaCheckPropertyChecks {
+  val keyGen: Gen[Key] =
+    Gen.oneOf(MajorKeyTest.majorKeyGen, MinorKeyTest.minorKeyGen)
+
+  val diatonicOps: Key => List[Note => Option[Note]] = (key: Key) =>
+    List(
+      DiatonicInterval.unison(_: Note)(key),
+      DiatonicInterval.second(_: Note)(key),
+      DiatonicInterval.third(_: Note)(key),
+      DiatonicInterval.fourth(_: Note)(key),
+      DiatonicInterval.fifth(_: Note)(key),
+      DiatonicInterval.sixth(_: Note)(key),
+      DiatonicInterval.seventh(_: Note)(key),
+      DiatonicInterval.octave(_: Note)(key)
+  )
+
+  property(
+    "a note that is enharmonic with any note in a key should return a " +
+      "new note for any diatonic method") {
+    forAll(for {
+      key <- keyGen
+      note <- NoteTest.noteGen
+    } yield (key, note)) {
+      case (key: Key, note: Note) =>
+        val nearestNotes = key.degrees.map(Note(_).get.nearestNote)
+
+        assert(
+          if (nearestNotes.exists(n => n.name == note.nearestNote.name))
+            diatonicOps(key).forall(op => op(note).isDefined)
+          else diatonicOps(key).forall(op => op(note).isEmpty))
+    }
+  }
+
+  property(
+    "diatonic intervals for major keys should result in major" +
+      "key interval distances between notes") {
+    forAll(MajorKeyTest.majorKeyGen) { key: Key =>
+      {
+        implicit val impKey: Key = key
+        val note = Note(key.tonic).get
+
+        val diatonicAndMajors = diatonicOps(key) zip
+          List(
+            note.perfect.unison,
+            note.major.second,
+            note.major.third,
+            note.perfect.fourth,
+            note.perfect.fifth,
+            note.major.sixth,
+            note.major.seventh,
+            note.perfect.octave
+          )
+
+        assert(diatonicAndMajors.forall(tup =>
+          Note.enharmonic(tup._1(note).get, tup._2)))
+      }
+    }
+  }
+
+  property(
+    "diatonic intervals for minor keys should result in minor" +
+      "key interval distances between notes") {
+    forAll(MinorKeyTest.minorKeyGen) { key: Key =>
+      {
+        implicit val impKey: Key = key
+        val note = Note(key.tonic).get
+
+        val diatonicAndMajors = diatonicOps(key) zip
+          List(
+            note.perfect.unison,
+            note.major.second,
+            note.minor.third,
+            note.perfect.fourth,
+            note.perfect.fifth,
+            note.minor.sixth,
+            note.minor.seventh,
+            note.perfect.octave
+          )
+
+        assert(diatonicAndMajors.forall(tup =>
+          Note.enharmonic(tup._1(note).get, tup._2)))
+      }
+    }
+  }
+
+  property(
+    "diatonic intervals should appropriately extract the scale degrees " +
+      "relative to the placement of a note"
+  ) {
+    forAll(for {
+      key <- keyGen
+      position <- Gen.chooseNum(0, 6)
+    } yield (key, position)) {
+      case (key: Key, position: Int) =>
+        val note = Note(key.degrees(position)).get
+
+        val diatonicAndOffset = diatonicOps(key) zip (0 to 7)
+        assert(
+          diatonicAndOffset.forall(tup =>
+            tup._1(note).get.name ==
+              key.degrees((position + tup._2) % key.degrees.size)))
+    }
+  }
+}
