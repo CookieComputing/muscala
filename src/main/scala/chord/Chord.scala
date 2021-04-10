d71692323536ca30e31e6b1847cf15131d69273d
Kevin Hui <khui.cs@gmail.com>
2021-04-09 21:05:37 -0400

    [Chord] Add toNotes() to chord interface

--- src/main/scala/chord/Chord.scala
+++ src/main/scala/chord/Chord.scala
@@ -1,4 +1,5 @@
 package chord
+import note.Note
 
 /**
   * A chord represents the Western notion of a chord: A collection of pitches
@@ -12,4 +13,13 @@ trait Chord {
   // The tones that can be found in a chord. Depending on the chord, the size
   // of this can vary
   val tones: List[String]
+
+  /**
+    * Given an octave that the tonic of the chord will be based on, returns a
+    * collection of notes that represent the chord. The default is centered
+    * around C4-B4.
+    * @param octave The octave that the root note should be based off of
+    * @return the chord as a list of notes
+    */
+  def toNotes(octave: Int = 4): List[Note]
 }
