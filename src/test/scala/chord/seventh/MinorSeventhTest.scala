bbce89d4722fdf8953b5e7b407a3c55d9bc1fd0f
Kevin Hui <khui.cs@gmail.com>
2021-04-11 21:13:06 -0400

    [MinorSeventh] Update property tests to use OptionValue

--- src/test/scala/chord/seventh/MinorSeventhTest.scala
+++ src/test/scala/chord/seventh/MinorSeventhTest.scala
@@ -4,10 +4,14 @@ import chord.triad.MinorTriad
 import key.{MinorKey, MinorKeyTest}
 import note.Note
 import org.scalacheck.Gen
+import org.scalatest.OptionValues
 import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class MinorSeventhTest extends AnyPropSpec with ScalaCheckPropertyChecks {
+class MinorSeventhTest
+    extends AnyPropSpec
+    with ScalaCheckPropertyChecks
+    with OptionValues {
   property("a minor seventh should have 4 chord tones") {
     forAll(minorSeventhChordGen) { seventh: MinorSeventh =>
       assertResult(4)(seventh.tones.size)
@@ -20,18 +24,20 @@ class MinorSeventhTest extends AnyPropSpec with ScalaCheckPropertyChecks {
     forAll(for {
       seventh <- minorSeventhChordGen
       key <- MinorKey(seventh.tonic)
-    } yield (seventh, key)) {
-      case Some((seventh: MinorSeventh, key: MinorKey)) =>
-        assert(
-          seventh.root == key.degrees.head &&
-            seventh.third == key.degrees(2) &&
-            seventh.fifth == key.degrees(4) &&
-            seventh.seventh == key.degrees(6) &&
-            seventh.tones == List(key.degrees.head,
-                                  key.degrees(2),
-                                  key.degrees(4),
-                                  key.degrees(6))
-        )
+    } yield (seventh, key)) { gen =>
+      gen.value match {
+        case (seventh: MinorSeventh, key: MinorKey) =>
+          assert(
+            seventh.root == key.degrees.head &&
+              seventh.third == key.degrees(2) &&
+              seventh.fifth == key.degrees(4) &&
+              seventh.seventh == key.degrees(6) &&
+              seventh.tones == List(key.degrees.head,
+                                    key.degrees(2),
+                                    key.degrees(4),
+                                    key.degrees(6))
+          )
+      }
     }
   }
 
@@ -39,8 +45,8 @@ class MinorSeventhTest extends AnyPropSpec with ScalaCheckPropertyChecks {
     "a minor seventh chord should have the intervals of a minor triad," +
       " along with a minor seventh interval") {
     forAll(minorSeventhChordGen) { seventh: MinorSeventh =>
-      val triad = MinorTriad(seventh.tonic).get
-      val triadNotes = triad.toNotes()
+      val triad = MinorTriad(seventh.tonic)
+      val triadNotes = triad.value.toNotes()
       ((triadNotes ++ List(triadNotes.head.minor.seventh)) zip
         seventh.toNotes()).forall((Note.enharmonic _).tupled)
     }
@@ -55,8 +61,8 @@ class MinorSeventhTest extends AnyPropSpec with ScalaCheckPropertyChecks {
   }
 }
 
-object MinorSeventhTest {
+object MinorSeventhTest extends OptionValues {
   val minorSeventhChordGen: Gen[MinorSeventh] = for {
     key <- MinorKeyTest.minorKeyGen
-  } yield MinorSeventh(key.tonic).get
+  } yield MinorSeventh(key.tonic).value
 }
