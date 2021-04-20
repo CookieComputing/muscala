8a18c62f5f494e735d5ee531a8b8a89171dbc975
Kevin Hui <khui.cs@gmail.com>
2021-04-19 23:35:43 -0400

    [HarmonicMinorScale] Add harmonic minor scale Closes #15

---
+++ src/main/scala/scale/HarmonicMinorScale.scala
@@ -0,0 +1,62 @@
+package scale
+import chord.seventh.{
+  AugmentedMajorSeventh,
+  DiminishedSeventh,
+  DominantSeventh,
+  MinorMajorSeventh,
+  Seventh
+}
+import chord.triad.{AugmentedTriad, DiminishedTriad, MajorTriad, Triad}
+import key.MinorKey
+import note.Note
+
+/**
+  * Represents a harmonic minor scale. A harmonic minor scale is a
+  * modification of the natural minor scale, but with the seventh scale degree
+  * raised by one sharp.
+  */
+case class HarmonicMinorScale private (tonic: String) extends Scale {
+  private val naturalMinorScale = NaturalMinorScale(tonic).get
+
+  override def ascending: List[String] = {
+    val degrees = naturalMinorScale.ascending
+    degrees.updated(6, Note.sharpName(degrees(6)))
+  }
+
+  override def toNotes(octave: Int): List[Note] = {
+    val notes = naturalMinorScale.toNotes(octave)
+    notes.dropRight(1) ++ List(
+      notes.last.sharp.clearConflictingAccidentals
+    )
+  }
+
+  /**
+    * Returns the triads that can be formed when starting at each scale
+    * degree. The index of each triad in the list corresponds to the scale
+    * degree used as the tonic of the triad.
+    * @return the list of triads for the scale
+    */
+  def triads: List[Triad] =
+    naturalMinorScale.triads
+      .updated(2, AugmentedTriad(ascending(2)).get)
+      .updated(4, MajorTriad(ascending(4)).get)
+      .updated(6, DiminishedTriad(ascending(6)).get)
+
+  /**
+    * Returns the sevenths that can be formed when starting at each scale
+    * degree. The index of each seventh in the list corresponds to the scale
+    * degree used as the tonic of the seventh.
+    * @return the list of sevenths for the scale
+    */
+  def sevenths: List[Seventh] =
+    naturalMinorScale.sevenths
+      .updated(0, MinorMajorSeventh(ascending.head).get)
+      .updated(2, AugmentedMajorSeventh(ascending(2)).get)
+      .updated(4, DominantSeventh(ascending(4)).get)
+      .updated(6, DiminishedSeventh(ascending(6)).get)
+}
+
+object HarmonicMinorScale {
+  def apply(tonic: String): Option[HarmonicMinorScale] =
+    if (MinorKey(tonic).isDefined) Some(new HarmonicMinorScale(tonic)) else None
+}
---
+++ src/test/scala/scale/HarmonicMinorScaleTest.scala
@@ -0,0 +1,165 @@
+package scale
+import chord.seventh.{
+  AugmentedMajorSeventh,
+  DiminishedSeventh,
+  DominantSeventh,
+  HalfDiminishedSeventh,
+  MajorSeventh,
+  MinorMajorSeventh,
+  MinorSeventh
+}
+import chord.triad.{AugmentedTriad, DiminishedTriad, MajorTriad, MinorTriad}
+import note.{Note, NoteTest}
+import org.scalacheck.Gen
+import org.scalatest.OptionValues
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import scale.HarmonicMinorScaleTest.harmonicMinorScaleGen
+
+class HarmonicMinorScaleTest extends AnyPropSpec with ScalaCheckPropertyChecks {
+  property(
+    "a harmonic minor scale should be a natural minor scale but with a" +
+      " raised seventh") {
+    forAll(for {
+      letter <- NoteTest.noteLetterGen
+      naturalMinor <- NaturalMinorScale(letter.toString)
+      harmonicMinor <- HarmonicMinorScale(letter.toString)
+    } yield (naturalMinor, harmonicMinor)) {
+      case Some(
+          (naturalMinor: NaturalMinorScale,
+           harmonicMinor: HarmonicMinorScale)) =>
+        assertResult(
+          naturalMinor.ascending.updated(6,
+                                         Note.sharpName(naturalMinor
+                                           .ascending(6))))(
+          harmonicMinor.ascending)
+      case _ => fail("expected successful scale generation")
+    }
+  }
+
+  property(
+    "the harmonic minor's gap between the sixth and seventh degree " +
+      "should be 3 half steps") {
+    forAll(for {
+      scale <- harmonicMinorScaleGen
+      octave <- Gen.chooseNum(-10000, 10000)
+    } yield (scale, octave)) {
+      case (scale: HarmonicMinorScale, octave: Int) =>
+        val notes = scale.toNotes(octave)
+        assertResult(3)(Note.distance(notes(5), notes(6)))
+    }
+  }
+
+  property(
+    "a harmonic minor scale should have the expected interval " +
+      "distances") {
+    forAll(for {
+      octave <- Gen.chooseNum(-10000, 10000)
+      scale <- harmonicMinorScaleGen
+    } yield (scale, octave)) {
+      case (scale: HarmonicMinorScale, octave: Int) =>
+        val notes = scale.toNotes(octave)
+        assert(
+          Note.enharmonic(notes.head, notes.head.perfect.unison) &&
+            Note.enharmonic(notes(1), notes.head.major.second) &&
+            Note.enharmonic(notes(2), notes.head.minor.third) &&
+            Note.enharmonic(notes(3), notes.head.perfect.fourth) &&
+            Note.enharmonic(notes(4), notes.head.perfect.fifth) &&
+            Note.enharmonic(notes(5), notes.head.minor.sixth) &&
+            Note.enharmonic(notes(6), notes.head.major.seventh)
+        )
+    }
+  }
+
+  property(
+    "a harmonic minor scale follows the pattern that the natural " +
+      "minor scale does, but the seventh is raised by one half step") {
+    forAll(for {
+      octave <- Gen.chooseNum(-10000, 10000)
+      scale <- harmonicMinorScaleGen
+    } yield (scale, octave)) {
+      case (scale: HarmonicMinorScale, octave: Int) =>
+        val notes = scale.toNotes(octave)
+        assert(
+          notes.head.rank + 2 == notes(1).rank &&
+            notes(1).rank + 1 == notes(2).rank &&
+            notes(2).rank + 2 == notes(3).rank &&
+            notes(3).rank + 2 == notes(4).rank &&
+            notes(4).rank + 1 == notes(5).rank &&
+            notes(5).rank + 3 == notes(6).rank &&
+            notes(6).rank + 1 == notes.head.perfect.octave.rank
+        )
+    }
+  }
+
+  property(
+    "a harmonic minor scale's descending order should be the reverse of " +
+      "it's ascending order") {
+    forAll(harmonicMinorScaleGen) { scale: HarmonicMinorScale =>
+      assert(scale.ascending.reverse == scale.descending)
+    }
+  }
+
+  property(
+    "a harmonic minor scale's triads should follow the expected chord triads"
+  ) {
+    forAll(harmonicMinorScaleGen) { minorScale: HarmonicMinorScale =>
+      minorScale.triads match {
+        case List(MinorTriad(_),
+                  DiminishedTriad(_),
+                  AugmentedTriad(_),
+                  MinorTriad(_),
+                  MajorTriad(_),
+                  MajorTriad(_),
+                  DiminishedTriad(_)) =>
+          succeed
+        case _ => fail("triad pattern was not matched")
+      }
+    }
+  }
+
+  property(
+    "the chord tones in each triad in a harmonic minor scales's triads should" +
+      " be scale degrees found in the minor scale") {
+    forAll(harmonicMinorScaleGen) { minorScale: HarmonicMinorScale =>
+      assert(minorScale.triads.forall(triad =>
+        triad.tones.toSet.subsetOf(minorScale.ascending.toSet)))
+    }
+  }
+
+  property(
+    "a harmonic minor key's seventh chords should follow the expected seventh" +
+      " chords"
+  ) {
+    forAll(harmonicMinorScaleGen) { scale: HarmonicMinorScale =>
+      scale.sevenths match {
+        case List(
+            MinorMajorSeventh(_),
+            HalfDiminishedSeventh(_),
+            AugmentedMajorSeventh(_),
+            MinorSeventh(_),
+            DominantSeventh(_),
+            MajorSeventh(_),
+            DiminishedSeventh(_)
+            ) =>
+          succeed
+        case _ => fail("seventh chord pattern was not matched")
+      }
+    }
+  }
+
+  property(
+    "the chord tones in each seventh in a harmonic minor scale's seventh " +
+      "should be scale degrees found in the harmonic minor scale") {
+    forAll(harmonicMinorScaleGen) { scale: HarmonicMinorScale =>
+      assert(scale.sevenths.forall(seventh =>
+        seventh.tones.toSet.subsetOf(scale.ascending.toSet)))
+    }
+  }
+}
+
+object HarmonicMinorScaleTest extends OptionValues {
+  val harmonicMinorScaleGen: Gen[HarmonicMinorScale] = for {
+    letter <- NoteTest.noteLetterGen
+  } yield HarmonicMinorScale(letter.toString).value
+}
