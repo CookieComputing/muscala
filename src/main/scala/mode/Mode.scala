e72158ce46cdaef4fc1986d2ad853f16a663bbe0
Kevin Hui <khui.cs@gmail.com>
2021-04-23 14:19:40 -0400

    [Mode] Add mode trait Closes #25

---
+++ src/main/scala/mode/Mode.scala
@@ -0,0 +1,9 @@
+package mode
+import scale.Scale
+
+/**
+  * Represents a modern Western mode. These modes use the same set of notes
+  * as the major scale, and are differentiated by the starting interval.
+  * There are multiple modes for each note.
+  */
+trait Mode extends Scale {}
