8ae2ed0337542f109f08f912659a713280da0deb
Kevin Hui <khui.cs@gmail.com>
2021-04-24 05:22:02 -0400

    [Lydian] Add lydian mode

--- src/test/scala/mode/LydianTest.scala
+++ src/test/scala/mode/LydianTest.scala
@@ -1,4 +1,5 @@
 package mode
+import chord.triad.{DiminishedTriad, MajorTriad, MinorTriad}
 import mode.LydianTest.lydianModeGen
 import note.Note
 import org.scalacheck.Gen
@@ -29,6 +30,17 @@ class LydianTest
     }
   }
 
+  property("the fourth should be an augmented fourth") {
+    forAll(for {
+      mode <- lydianModeGen
+      octave <- Gen.chooseNum(-10000, 10000)
+    } yield (mode, octave)) {
+      case (mode: Lydian, octave: Int) =>
+        val notes = mode.toNotes(octave)
+        assert(Note.enharmonic(notes.head.augmented.fourth, notes(3)))
+    }
+  }
+
   property("the lydian mode should follow the W-W-W-H-W-W-H pattern") {
     forAll(for {
       mode <- lydianModeGen
@@ -45,6 +57,52 @@ class LydianTest
             Note.distance(notes(5), notes(6)) == 2 &&
             Note.distance(notes(6), notes.head.perfect.octave) == 1
         )
+    }
+  }
+
+  property("the tonic, dominant, and supertonic are all major triads") {
+    forAll(lydianModeGen) { mode: Lydian =>
+      val tonic = MajorTriad(mode.tonic).value
+      val dominant = MajorTriad(mode.ascending(4)).value
+      val supertonic = MajorTriad(mode.ascending(1)).value
+
+      assert(
+        tonic.tones == List(mode.ascending.head,
+                            mode.ascending(2),
+                            mode.ascending(4)) &&
+          dominant.tones == List(mode.ascending(4),
+                                 mode.ascending(6),
+                                 mode.ascending(1)) &&
+          supertonic.tones == List(mode.ascending(1),
+                                   mode.ascending(3),
+                                   mode.ascending(5))
+      )
+    }
+  }
+
+  property("the subdominant should be diminished") {
+    forAll(lydianModeGen) { mode: Lydian =>
+      assertResult(DiminishedTriad(mode.ascending(3)).value.tones)(
+        List(mode.ascending(3), mode.ascending(5), mode.ascending.head))
+    }
+  }
+
+  property("the leading tone, mediant, and submediant are all minor") {
+    forAll(lydianModeGen) { mode: Lydian =>
+      val leadingTone = MinorTriad(mode.ascending(6)).value
+      val mediant = MinorTriad(mode.ascending(2)).value
+      val submediant = MinorTriad(mode.ascending(5)).value
+      assert(
+        leadingTone.tones == List(mode.ascending(6),
+                                  mode.ascending(1),
+                                  mode.ascending(3)) &&
+          mediant.tones == List(mode.ascending(2),
+                                mode.ascending(4),
+                                mode.ascending(6)) &&
+          submediant.tones == List(mode.ascending(5),
+                                   mode.ascending.head,
+                                   mode.ascending(2))
+      )
     }
   }
 }
